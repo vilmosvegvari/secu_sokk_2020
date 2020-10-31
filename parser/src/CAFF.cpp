d21d7b8774ac4f261c24c084f995d0ce11394e48
Dávid Gracza <graczadavid@gmail.com>
2020-10-31 19:15:46 +0100

    Add read frames

--- parser/src/CAFF.cpp
+++ parser/src/CAFF.cpp
@@ -9,9 +9,11 @@ CAFF::CAFF(std::string path) : path(std::move(path)) {
 }
 
 void CAFF::parseCaff() {
-    std::ifstream caffFile(this->path, std::ios::out | std::ios::binary);
+    std::ifstream caffFile(this->path, std::ios::binary);
     if (caffFile.is_open()) {
-
+        while (caffFile.peek() != EOF) {
+            readFrame(caffFile);
+        }
     }
     caffFile.close();
 }
@@ -20,3 +22,47 @@ std::string CAFF::getFileName() {
     return fs::path(this->path).stem();
 }
 
+void CAFF::readFrame(std::ifstream &file) {
+    const auto frameId = readFrameID(file);
+    const int64_t length = readLength(file);
+    auto data = readData(file, length);
+    switch (frameId) {
+        case HEADER:
+            parseHeader(data);
+            break;
+        case CREDITS:
+            parseCredits(data);
+            break;
+        case ANIMATION:
+            parseAnimation(data);
+            break;
+    }
+}
+
+CAFF::FrameID CAFF::readFrameID(std::ifstream &file) {
+    return FrameID(file.get());
+}
+
+int64_t CAFF::readLength(std::ifstream &file) {
+    int64_t number;
+    file.read(reinterpret_cast<char *>(&number), 8);
+    return number;
+}
+
+std::vector<char> CAFF::readData(std::ifstream &file, const int64_t length) {
+    auto data = std::vector<char>(length);
+    file.read(reinterpret_cast<char *>(data.data()), length);
+    return data;
+}
+
+void CAFF::parseHeader(std::vector<char> data) {
+
+}
+
+void CAFF::parseCredits(std::vector<char> data) {
+
+}
+
+void CAFF::parseAnimation(std::vector<char> data) {
+
+}
--- parser/src/CAFF.hpp
+++ parser/src/CAFF.hpp
@@ -3,16 +3,38 @@
 
 
 #include <string>
+#include <vector>
 
 class CAFF {
-    std::string path;
+    const std::string path;
+
+    enum FrameID : unsigned char {
+        HEADER = 1,
+        CREDITS = 2,
+        ANIMATION = 3
+    };
+
+    void readFrame(std::ifstream &file);
+
+    static FrameID readFrameID(std::ifstream &file);
+
+    static int64_t readLength(std::ifstream &file);
+
+    static std::vector<char> readData(std::ifstream &file, int64_t length);
 
 public:
+
     explicit CAFF(std::string path);
 
     void parseCaff();
 
     std::string getFileName();
+
+    void parseHeader(std::vector<char> data);
+
+    void parseCredits(std::vector<char> data);
+
+    void parseAnimation(std::vector<char> data);
 };
 
 
