77b8cc9fb029bee8239edf38d4f776162d54460b
Végvári Vilmos <vegvari.vili@gmail.com>
2020-11-11 17:36:06 +0100

    F - basic routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AuthComponent } from './auth/auth.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { UploadComponent } from './upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent },
+  { path: 'pictures', component: PicturesComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'upload', component: UploadComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
