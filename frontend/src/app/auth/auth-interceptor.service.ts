4efcb64b6f3a541e0b3d3a8de4b02f2d1651bdac
Végvári Vilmos <vegvari.vili@gmail.com>
2020-11-13 23:16:12 +0100

    F - token moved from queryparam to header and url fix

--- frontend/src/app/auth/auth-interceptor.service.ts
+++ frontend/src/app/auth/auth-interceptor.service.ts
@@ -26,7 +26,7 @@ export class AuthInterceptorService implements HttpInterceptor {
           return next.handle(req);
         }
         const modifiedReq = req.clone({
-          params: new HttpParams().set('auth', user.token),
+          setHeaders: { Authorization: user.token },
         });
         return next.handle(modifiedReq);
       })
