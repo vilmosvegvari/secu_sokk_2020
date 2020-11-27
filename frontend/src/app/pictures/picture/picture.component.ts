435d7873a905ed97f02bbe9dcd3a435f2894332e
Ötvös Máté <mati9606@gmail.com>
2020-11-27 21:03:06 +0100

    picture componenet base

--- frontend/src/app/pictures/picture/picture.component.ts
+++ frontend/src/app/pictures/picture/picture.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PictureService } from './picture.service';
 
 @Component({
   selector: 'app-picture',
@@ -10,12 +11,17 @@ export class PictureComponent implements OnInit, OnDestroy {
   id: number;
   private sub: any;
 
-  constructor(private route: ActivatedRoute) { }
+  picture = null;
+
+  constructor(private pictureService: PictureService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.sub = this.route.params.subscribe(params => {
       this.id = +params['id'];
    });
+
+   this.pictureService.picture.subscribe((picture) => (this.picture = picture));
+   this.pictureService.fetchPicture(this.id);
   }
 
   ngOnDestroy() {
--- frontend/src/app/pictures/picture/picture.service.ts
+++ frontend/src/app/pictures/picture/picture.service.ts
@@ -1,18 +1,35 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 interface PictureDetailResponse {
-  id : number,
-  name: string,
-  filename: string,
-  filesize: number
+  id : number
 }
 
 @Injectable({ providedIn: 'root' })
 export class PictureService {
-  pictures = new BehaviorSubject<PictureDetailResponse>(null);
+  picture = new BehaviorSubject<PictureDetailResponse>(null);
 
   constructor(private http: HttpClient) {}
 
+  fetchPicture(pictureId) {
+    console.log('fetching');
+    return this.http
+      .get<PictureDetailResponse>(environment.apiUrl + `/details/${pictureId}`)
+      .pipe(
+        map((picture) => {          
+            return {
+              id: picture.id
+            };
+          }
+        ),
+        tap((picture) => {
+          console.log(picture);
+          this.picture.next(picture);
+        }) 
+        )
+      .subscribe();
+  }
 }
